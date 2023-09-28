871904561012cc437dab0505d7e849379d551b9d
Diamond006 <145776440+Diamond006@users.noreply.github.com>
2023-09-28 13:11:52 +0500

    Add files via upload

--- input.js
+++ input.js
@@ -1,1 +1,17 @@
 
+$(window).scroll(function () {
+    if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50) {
+        $('.nc_container').css('transition', 'all .5s ease');
+        $('.nc_container').css('zIndex', '122');
+        $('.nc_container').css('backgroundColor', 'gray')
+    }
+     else {
+        $('.nc_container').css('transition', 'all .5s ease');
+        $('.nc_container').css('backgroundColor', 'transparent')
+    }
+})
+
+
+
+
+// $('.nc_container').css('color', 'red'); 
